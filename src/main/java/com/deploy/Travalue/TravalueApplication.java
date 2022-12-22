094675dbcc6ecbb9090a58a0c1c841a7f3fad947
Crayon <crayon@iseungheon-ui-MacBookPro.local>
2022-12-22 21:53:44 +0900

    [Setting]: 프로젝트 기본 세팅
    build.gradle 기본 세팅
    JPA & MySQL 연동
    
    Related to: #1

---
+++ src/main/java/com/deploy/Travalue/TravalueApplication.java
@@ -0,0 +1,11 @@
+package com.deploy.Travalue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravalueApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TravalueApplication.class, args);
+	}
+}
---
+++ src/test/java/com/deploy/Travalue/TravalueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.Travalue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravalueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
