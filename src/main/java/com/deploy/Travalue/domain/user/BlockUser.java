3606457b8c3264c9b7927457e626911915118075
Lee SeungHeon <lsh328328@naver.com>
2023-03-07 13:49:39 +0900

    [setting]: Entity AuditingTimeEntity 추가

--- src/main/java/com/deploy/Travalue/domain/user/BlockUser.java
+++ src/main/java/com/deploy/Travalue/domain/user/BlockUser.java
@@ -1,5 +1,6 @@
 package com.deploy.Travalue.domain.user;
 
+import com.deploy.Travalue.domain.common.AuditingTimeEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class BlockUser {
+public class BlockUser extends AuditingTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
