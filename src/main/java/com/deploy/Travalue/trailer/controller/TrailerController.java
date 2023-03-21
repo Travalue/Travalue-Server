de4e66c5cbf96ea3a07798bd8c3175609f0c1614
seohyun-106 <changseohyun@sookmyung.ac.kr>
2023-03-21 22:11:45 +0900

    Feat: Trailer controller, service 파일 추가

---
+++ src/main/java/com/deploy/Travalue/trailer/controller/TrailerController.java
@@ -0,0 +1,9 @@
+package com.deploy.Travalue.trailer.controller;
+
+import com.deploy.Travalue.trailer.service.TrailerService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class TrailerController {
+    private final TrailerService trailerService;
+}
---
+++ src/main/java/com/deploy/Travalue/trailer/service/TrailerService.java
@@ -0,0 +1,4 @@
+package com.deploy.Travalue.trailer.service;
+
+public class TrailerService {
+}
