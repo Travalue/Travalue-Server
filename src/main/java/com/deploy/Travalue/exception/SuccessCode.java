6c38da7a26fa7b82206d8619154bee9cae7f014b
seohyun-106 <changseohyun@sookmyung.ac.kr>
2023-05-26 21:05:27 +0900

    Feat: Traveller 상세조회 성공코드 추가

--- src/main/java/com/deploy/Travalue/exception/SuccessCode.java
+++ src/main/java/com/deploy/Travalue/exception/SuccessCode.java
@@ -23,6 +23,8 @@ public enum SuccessCode {
 
     READ_TRAILERS_SUCCESS(HttpStatus.OK, "트레일러 전체 조회 성공"),
     READ_TRAVELLERS_SUCCESS(HttpStatus.OK, "트레블러 전체 조회 성공"),
+
+    READ_TRAVEL_SUCCESS(HttpStatus.OK, "트레블러/트레일러 상세 조회 성공"),
     ;
 
     private final HttpStatus httpStatus;
