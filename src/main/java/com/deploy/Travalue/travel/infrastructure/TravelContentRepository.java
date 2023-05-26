1b5e00094f304861d6cba300f384df46ec7b7732
seohyun-106 <changseohyun@sookmyung.ac.kr>
2023-05-26 21:09:23 +0900

    Feat: TravelContent, TravelPin Repository 추가

---
+++ src/main/java/com/deploy/Travalue/travel/infrastructure/TravelContentRepository.java
@@ -0,0 +1,10 @@
+package com.deploy.Travalue.travel.infrastructure;
+
+import com.deploy.Travalue.travel.domain.TravelContent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TravelContentRepository extends JpaRepository<TravelContent, Long> {
+    List<TravelContent> findTravelContentByTravelId(Long travelId);
+}
